39d652bec95ca54f852c04fa07370bd0b2948d01
Muhammad zulfatah <zfath423@gmail.com>
2024-10-29 10:14:56 +0700

    fix

--- Mini-Proyek2/main.js
+++ Mini-Proyek2/main.js
@@ -2,22 +2,36 @@
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
 
+function showAlert() {
+    Swal.fire('Terima Kasih', 'Pesan Anda telah terkirim', 'success');
+}
+
+function showAlertError() {
+    Swal.fire('Maaf', 'Pesan Anda tidak terkirim', 'error');
+}
+
+
+
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
 
 
-
 document.getElementById('messageForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault(); // Mencegah form submit secara default
 
-    // Get form values
+    // Mengambil tombol dan mengganti teks menjadi "Loading..."
+    const sendButton = document.querySelector('.btn-send');
+    sendButton.textContent = 'Loading...';
+    sendButton.disabled = true; // Nonaktifkan tombol sementara
+
+    // Ambil nilai form
     const name = document.getElementById('name').value;
     const subject = document.getElementById('subject').value;
     const text = document.getElementById('text').value;
 
-    // Create JSON object
+    // Buat objek JSON
     const messageData = {
         "to": "zfath423@gmail.com",
         "name": name,
@@ -25,7 +39,7 @@ document.getElementById('messageForm').addEventListener('submit', function(event
         "text": text
     };
 
-    // Send the data to API using fetch
+    // Mengirim data ke API menggunakan fetch
     fetch('https://lumoshive-academy-email-api.vercel.app/send-email', {
         method: 'POST',
         headers: {
@@ -36,14 +50,20 @@ document.getElementById('messageForm').addEventListener('submit', function(event
     })
     .then(response => response.json())
     .then(data => {
-        alert('Pesan Anda telah dikirim!');
+        showAlert(); // Tampilkan alert sukses
         console.log('Success:', data);
     })
     .catch((error) => {
-        alert('Error sending message!');
+        showAlertError(); // Tampilkan alert error
         console.error('Error:', error);
+    })
+    .finally(() => {
+        // Kembalikan teks tombol ke "Send Message" setelah selesai
+        sendButton.textContent = 'Send Message';
+        sendButton.disabled = false; // Aktifkan kembali tombol
     });
 });
+
 
 // Show or hide the button based on scroll position
 window.onscroll = function() { scrollFunction() };
